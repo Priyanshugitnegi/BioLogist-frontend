04b4be7537f1aa7a7f48437f064b145f38631c28
Priyanshugitnegi <negips11april@gmail.com>
2025-12-30 02:18:52 +0530

    Fix utils/auth path casing for Vercel

--- src/utils/auth.js
+++
@@ -1,9 +0,0 @@
-export const isAuthenticated = () => {
-    return !!localStorage.getItem("access");
-  };
-  
-  export const logout = () => {
-    localStorage.removeItem("access");
-    localStorage.removeItem("refresh");
-  };
-  
